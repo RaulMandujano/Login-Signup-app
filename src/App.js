b6bec4ac772196d03270837e0dca24bd482c1b2a
Raul Mandujano <raulmandujanoleon@gmail.com>
2020-12-10 22:22:03 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./components/login.component";
+import SignUp from "./components/signup.component";
+
+function App() {
+  return (<Router>
+    <div className="App">
+      <nav >
+            <ul>
+              <li >
+                <Link className="nav-link" to={"/sign-in"}>Login</Link>
+              </li>
+              <li >
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>        
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
+  );
+}
+
+export default App;
